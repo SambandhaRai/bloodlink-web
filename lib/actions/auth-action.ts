72586c5a45da5fb605b47c45fb3ae727113b446d
SambandhaRai <ginny_30@icloud.com>
2026-01-20 16:23:00 +0545

    Created Actions for Auth and Blood Group Fetching

---
+++ lib/actions/auth-action.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { loginUser, registerUser } from "../api/auth";
+
+export const handleRegister = async (formData: any) => {
+    try{
+        const result = await registerUser(formData);
+        if(result.success) {
+            return {
+                success: true,
+                data: result.data,
+                message: "Registration Successful"
+            };
+        }
+        return {
+            success: false,
+            message: result.message || "Registration Failed"
+        };
+    } catch (err: Error | any) {
+        console.log("HANDLE REGISTER ERROR:", err.response?.data);
+        return {
+            success: false,
+            message: err.message || "Registration Failed"
+        };
+    }
+}
+
+export const handleLogin = async (formData: any) => {
+    try {
+        const result = await loginUser(formData);
+        if(result.success) {
+            return {
+                success: true,
+                data: result.data,
+                message: "Login Successful"
+            };
+        }
+        return {
+            success: false,
+            message: result.message || "Login Failed"
+        }
+    } catch (err: Error | any) {
+        return {
+            success: false,
+            message: err.message || "Login Failed"
+        };
+    }
+}
---
+++ lib/api/bloodGroup.ts
@@ -0,0 +1,28 @@
+import axios from "./axios";
+import { API } from "./endpoints";
+
+export const getAllBloodGroups = async () => {
+    try{
+        const res = await axios.get(API.BLOODGROUP.GETALL);
+        return res.data;
+    } catch (err: Error | any) {
+        return(
+            err.response?.data?.message 
+            || err.message
+            || "Failed to Fetch Blood Groups"
+        );
+    }
+}
+
+export const getBloodGroupById = async (id: string) => {
+    try{
+        const res = await axios.get(API.BLOODGROUP.GETBYID(id));
+        return res.data;
+    } catch (err: Error | any) {
+        return(
+            err.response?.data?.message 
+            || err.message
+            || "Failed to Fetch Blood Group"
+        );
+    }
+}
