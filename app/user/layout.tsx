df61ea51e2f7f8e6dfdf84c75de18b18ec2a2de9
SambandhaRai <ginny_30@icloud.com>
2026-02-21 23:00:55 +0545

    Updated Header to add more nav links and Added Footer to layout

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,10 +1,12 @@
+import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full bg-white">
             <Header />
             {children}
+            <Footer />
         </div>
     );
 }
